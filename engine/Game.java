b18f20bd109aef5593ff479e1a53a494a39756f9
sp4rkii <awael2468@hotmail.com>
2023-07-06 02:46:05 +0300

    ok

--- engine/Game.java
+++ engine/Game.java
@@ -8,17 +8,18 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Game {
+    private ArrayList<Ships> Availableships = new ArrayList<Ships>();
     private ArrayList<Ships> ships = new ArrayList<Ships>();
-    private Ships ship;
+    private Ships ship = new Empty();
     private Map Emap;
     private Map map;
 
     public Game() {
-        ships.add(new Carrier());
-        ships.add(new Cruiser());
-        ships.add(new Destroyer());
-        ships.add(new LongShip());
-        ships.add(new Submarine());
+        Availableships.add(new Carrier());
+        Availableships.add(new Cruiser());
+        Availableships.add(new Destroyer());
+        Availableships.add(new LongShip());
+        Availableships.add(new Submarine());
         Emap = new Map();
         map = new Map();
     }
@@ -31,7 +32,7 @@ public class Game {
         this.ship = ship;
     }
 
-    public void rotateShip() throws outOfBoundsException,overlapException {
+    public void rotateShip() throws outOfBoundsException, overlapException {
         ship.rotate();
         Point[] Coordinates = ship.getCoordinates();
         for (int i = 0; i < ship.getCoordinates().length; i++) {
@@ -40,13 +41,13 @@ public class Game {
             if (y < 0 || y > 14 || x < 0 || x > 14) {
                 throw new outOfBoundsException("Invalid coordinates");
             }
-            for(int j=0;j<ships.size();j++){
-                Ships temp=ships.get(j);
-                if(!(ship.getClass() == temp.getClass())){
-                    for(int k=0;k<temp.getCoordinates().length;k++){
-                         int m = (int) Coordinates[k].getX();
-                         int n = (int) Coordinates[k].getY();
-                        if(x==m && y==n){
+            for (int j = 0; j < ships.size(); j++) {
+                Ships temp = ships.get(j);
+                if (!(ship.getClass() == temp.getClass())) {
+                    for (int k = 0; k < temp.getCoordinates().length; k++) {
+                        int m = (int) Coordinates[k].getX();
+                        int n = (int) Coordinates[k].getY();
+                        if (x == m && y == n) {
                             throw new overlapException("Overlapped ships");
                         }
                     }
@@ -63,5 +64,20 @@ public class Game {
             }
         }
     }
-    
+
+    public void addShip() throws outOfBoundsException, overlapException {
+        if (!(ship instanceof Empty)) {
+            Availableships.remove(ship);
+            ships.add(ship);
+            ship.rotate();
+            rotateShip();
+            Point[] Coordinates = ship.getCoordinates();
+            for (int i = 0; i < ship.getCoordinates().length; i++) {
+                int x = (int) Coordinates[i].getX();
+                int y = (int) Coordinates[i].getY();
+                map.getMap()[x][y]=ship;
+            }
+            ship=new Empty();
+        }
+    }
 }
