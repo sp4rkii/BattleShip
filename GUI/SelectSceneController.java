125b6a59b692698b6b5d87c031c761af89927796
marwan <marwanmostafa20002000@gmail.com>
2023-07-07 12:08:19 +0300

    GUI

--- GUI/SelectSceneController.java
+++ GUI/SelectSceneController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import java.io.IOException;
@@ -13,6 +18,61 @@ public class SelectSceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    ImageView Carrier;
+    @FXML
+    ImageView Cruiser;
+    @FXML
+    ImageView Destroyer;
+    @FXML
+    ImageView LongShip;
+    @FXML
+    ImageView Submarine;
+
+    @FXML
+    private void DragDetectionCarrier(MouseEvent event) {
+        Dragboard db = Carrier.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent cb = new ClipboardContent();
+        cb.putImage(Carrier.getImage());
+        db.setContent(cb);
+        event.consume();
+    }
+
+    @FXML
+    private void DragDetectionCruiser(MouseEvent event) {
+        Dragboard db = Cruiser.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent cb = new ClipboardContent();
+        cb.putImage(Cruiser.getImage());
+        db.setContent(cb);
+        event.consume();
+    }
+
+    @FXML
+    private void DragDetectionDestroyer(MouseEvent event) {
+        Dragboard db = Destroyer.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent cb = new ClipboardContent();
+        cb.putImage(Destroyer.getImage());
+        db.setContent(cb);
+        event.consume();
+    }
+
+    @FXML
+    private void DragDetectionLongShip(MouseEvent event) {
+        Dragboard db = LongShip.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent cb = new ClipboardContent();
+        cb.putImage(LongShip.getImage());
+        db.setContent(cb);
+        event.consume();
+    }
+
+    @FXML
+    private void DragDetectionSubmarine(MouseEvent event) {
+        Dragboard db = Submarine.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent cb = new ClipboardContent();
+        cb.putImage(Submarine.getImage());
+        db.setContent(cb);
+        event.consume();
+    }
 
     @FXML
     public void SwitchToSelectScene2(ActionEvent event) throws IOException {
